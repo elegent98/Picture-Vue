3c6218fb0f0690c9588297bc3459223c7ec3daa7
Bobur <bobur@email.com>
2024-04-12 22:16:31 +0500

    Picture

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template >
+  <div class="flex justify-center items-center min-h-screen">
+    <Camera/>
+  </div>
+</template>
+<script>
+import Camera from "./components/Camera.vue"
+export default {
+  components:{
+    Camera
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import './output.css'
+import App from './App.vue'
+createApp(App).mount('#app')
